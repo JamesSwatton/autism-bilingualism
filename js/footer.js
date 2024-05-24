d0bd3cc02ea9a1b2f9c98251947ec642aa51d61b
JamesSwatton <jamessswatton@gmail.com>
2024-05-24 15:03:19 +0100

    update header code for all files

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,54 @@
+const footerWrapper = document.createElement('div');
+footerWrapper.className = 'footer-wrapper';
+footerWrapper.innerHTML = `
+			<footer>
+				<div>
+					<div class="logos">
+						<div class="logo" id="logo-1"><img src="assets/logos/autism_europe__logo.png" alt="Autism Europe logo">
+						</div>
+						<div class="logo" id="logo-2"><img src="assets/logos/qmu__logo.png" alt="Queen Margaret University logo">
+						</div>
+						<div class="logo" id="logo-3"><img src="assets/logos/edi_uni__logo.png" alt="Edinburgh University
+							logo"></div>
+						<div class="logo" id="logo-4"><img src="assets/logos/firah__logo.png" alt="Firah logo"></div>
+						<div class="logo" id="logo-5"><img src="assets/logos/bilingualism_matters__logo.png"
+								alt="Bilingualism Matters logo"></div>
+					</div>
+					<p class="footer--copyright">© Autism and Bilingualism</p>
+				</div>
+				<nav class="nav-footer">
+					<ul>
+						<li><a href="families/">Families</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+						<li><a href="practitioners/">Practitioners</a></li>
+					</ul>
+				</nav>
+				<div>
+					<button class="back-to-top" aria-label="Back to top">
+						<img src="assets/icons/back_to_top.svg" alt="back to top">
+					</button>
+					<div class="socials-wrapper">
+						<div class="social">
+							<a href="https://www.facebook.com" target="_blank"><img src="assets/logos/facebook__logo.png"
+									alt="facebook logo"></a>
+						</div>
+						<div class="social">
+							<a href="https://www.x.com/rachaelvdavis" target="_blank"><img src="assets/logos/x__logo.png" alt="x
+							logo"></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+`
+
+// add footer
+main.after(footerWrapper);
+
 const scrollTopBtn = document.querySelector('.back-to-top');
 
 scrollTopBtn.addEventListener('click', () => {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 })
+
--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,103 @@
+const body = document.querySelector('body');
+const header = document.createElement('header');
+header.className = 'nav-container';
+header.innerHTML = `
+		<div class="logo-wrapper">
+			<a href="index.html"><img src="assets/logo-large-01.svg" /></a>
+		</div>
+		<div class="nav-wrapper">
+			<div class="nav">
+				<nav>
+					<ul>
+						<li><a href="families/index.html">Families</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+						<li><a href="practitioners/index.html">Practitioners</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div class="selected-language">
+			<p id="language">English</p>
+			<div class="lang-icon language-select-btn" role="button" aria-pressed="false">
+				<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+					<path
+						d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z" />
+				</svg>
+			</div>
+		</div>
+		<div class="languages-wrapper">
+			<ul class="languages-list">
+				<li><a href="families/arabic.html">Arabic / عربي</a></li>
+				<li><a href="families/bengali.html">Bengali</a></li>
+				<li><a href="families/chinese.html">Chinese / 中国人</a></li>
+				<li><a href="families/">English</a></li>
+				<li><a href="families/french.html">French / Français</a></li>
+				<li><a href="families/greek.html">Greek / Ελληνικά</a></li>
+				<li><a href="families/italian.html">Italian / Italiano</a></li>
+				<li><a href="families/polish.html">Polish / Polski</a></li>
+				<li><a href="families/punjabi.html">Punjab / ਪੰਜਾਬ</a></li>
+				<li><a href="families/romanian.html">Romanian / Română</a></li>
+				<li><a href="families/scottish-gaelic.html">Scottish Gaelic / Gàidhlig na h-Alba</a></li>
+				<li><a href="families/somali.html">Somali / Soomaali</a></li>
+				<li><a href="families/spanish.html">Spanish / Español</a></li>
+				<li><a href="families/turkish.html">Turkish / Türkçe</a></li>
+				<li><a href="families/ukrainian.html">Ukranian / українська</a></li>
+				<li><a href="families/urdu.html">Urdu / اردو</a></li>
+				<li><a href="families/welsh.html">Welsh / Cymraeg</a></li>
+			</ul>
+		</div>
+		<!--Mobile version - hamburger and sidebar-->
+		<label class="hamburger-menu">
+			<input type="checkbox" id="menu-checkbox">
+		</label>
+
+		<aside class="sidebar">
+			<nav>
+				<ul class="nav__mobile">
+					<li>
+						<a href="families/">
+							<div class="resources-wrapper">
+								<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+									<path
+										d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z" />
+								</svg>
+								Families
+							</div>
+						</a>
+					</li>
+					<ul class="languages-list__mobile">
+						<li><a href="families/arabic.html">Arabic / عربي</a></li>
+						<li><a href="families/bengali.html">Bengali</a></li>
+						<li><a href="families/chinese.html">Chinese / 中国人</a></li>
+						<li><a href="families/">English</a></li>
+						<li><a href="families/french.html">French / Français</a></li>
+						<li><a href="families/greek.html">Greek / Ελληνικά</a></li>
+						<li><a href="families/italian.html">Italian / Italiano</a></li>
+						<li><a href="families/polish.html">Polish / Polski</a></li>
+						<li><a href="families/punjabi.html">Punjab / ਪੰਜਾਬ</a></li>
+						<li><a href="families/romanian.html">Romanian / Română</a></li>
+						<li><a href="families/scottish-gaelic.html">Scottish Gaelic / Gàidhlig na h-Alba</a></li>
+						<li><a href="families/somali.html">Somali / Soomaali</a></li>
+						<li><a href="families/spanish.html">Spanish / Español</a></li>
+						<li><a href="families/turkish.html">Turkish / Türkçe</a></li>
+						<li><a href="families/ukrainian.html">Ukranian / українська</a></li>
+						<li><a href="families/urdu.html">Urdu / اردو</a></li>
+						<li><a href="families/welsh.html">Welsh / Cymraeg</a></li>
+					</ul>
+					<li><a href="about.html">About</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<li><a href="practitioners/">Practioners</a></li>
+			</nav>
+		</aside>
+`
+// add nav to body
+body.prepend(header);
+
 // Show/hide header on scroll
 // Add padding top to doc__toc 
 //
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 const headerHeight = header.offsetHeight;
 const docToc = document.querySelector('.document__toc');
 let lastScrollTop = 0;
@@ -27,7 +123,6 @@ function scrollHide() {
 
 // Add margin top to body equalling header height
 //
-const body = document.querySelector('body');
 
 body.style.marginTop = `${header.offsetHeight}px`;
 
@@ -85,6 +180,29 @@ if (!regex.test(pathname)) {
 	langWrapper.remove();
 }
 
+// Add selected language text to element
+const langContainer = document.querySelector('#language');
+
+function formatFilePath(filePath) {
+	let a;
+	a = filePath.split('/').pop().split('.')[0].split('-');
+	a = a.map(word => {
+		return word.replace(word.charAt(0), word.charAt(0).toUpperCase());
+	})
+	return a.join(' ');
+}
+const selectedLanguage = formatFilePath(pathname);
+
+
+console.log(selectedLanguage);
+if (regex.test(pathname)) {
+	if (!selectedLanguage || selectedLanguage == 'Index') {
+		langContainer.innerHTML = "English";
+	} else {
+		langContainer.innerHTML = selectedLanguage;
+	}
+}
+
 // collapsible containers in document authors
 let containers = document.querySelectorAll('.collapsible-container');
 
@@ -96,3 +214,6 @@ containers.forEach(container => {
 		container.classList.toggle('opened');
 	})
 })
+
+
+
